c158f4fff20fdc7f84f6567d44b0e0288d3d5744
Iego Ribeiro <iego_aguiar@hotmail.com>
2020-06-20 19:47:57 -0300

    POPULANDO BANCO

--- src/java/web/DbListener.java
+++ src/java/web/DbListener.java
@@ -67,8 +67,40 @@ public class DbListener implements ServletContextListener {
                     + ")");
             
             if(Question.getList().isEmpty()){
+                //INSERT INTO questions VALUES ('Pergunta?', 'CERTA', 'ERRADA', 'ERRADA OPCIONAL', CATEGORIA OPCIONAL);
                 step = "Default questions creation";
-                stmt.executeUpdate("INSERT INTO questions VALUES ('Quanto é 1+1?', '1', 'dois', '3', 2)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('Normalmente, quantos litros de sangue uma pessoa tem?', 'Entre 4 a 6 litros.', 'Tem entre 2 a 4 litros.', 'Tem 7 litros', NULL)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('Em média, quantos litros são retirados numa doação de sangue?', 'São retirados 450 mililitros', 'São retirados 0,5 litro', 'São retirados 1,5 litros', NULL)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('De onde é a invenção do chuveiro elétrico?', 'Brasil', 'Inglaterra', 'França', NULL)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('Qual o nome do presidente do Brasil que ficou conhecido como Jango?', 'João Goulart', 'Jânio Quadros', 'Getúlio Vargas', NULL)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('Atualmente, quantos elementos químicos a tabela periódica possui?', '118', '109', '113', 2)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('Qual o número mínimo de jogadores de um time numa partida de futebol?', '7', '11', '9', 1)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('Quanto tempo a luz do Sol demora para chegar à Terra?', '8 minutos', '12 horas', '6 segundos', 2)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('Qual a altura da rede de vôlei nos jogos masculino e feminino?', '2,43 m e 2,24 m', '2,45 m e 2,15 m', '1,8 m e 1,5 m', 1)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('Em que ordem surgiram os modelos atômicos?', 'Dalton, Thomson, Rutherford, Rutherford-Bohr.', 'Rutherford-Bohr, Rutherford, Thomson, Dalton', 'Dalton, Thomson, Rutherford-Bohr, Rutherford', 2)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('Em que período da pré-história o fogo foi descoberto?', 'Paleolítico', 'Idade Média', 'Neolítico', NULL)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('Qual a montanha mais alta do Brasil?', 'Pico da Neblina', 'Monte Roraima', 'Monte Everest', NULL)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('Qual a velocidade da luz?', 'Aproximadamente 300.000.000 (m/s)'', 'Aproximadamente 150.000.000 (m/s)', 'Aproximadamente 30.000.000 (m/s)', 2)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('“Twenty past six”. Que horas são em inglês?', '6:20', '12:06', '12:54', NULL)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('Quem é o autor de “O Príncipe”?', 'Maquiavel', 'Antoine de Saint-Exupéry', 'Rousseau', NULL)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('Quantos graus são necessário para que dois ângulos sejam complementares?', '90', '45', '180', 2)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('Se uma casa tem quatro lados e em cada canto tem um gato e cada gato vê três gatos, quantos gatos há na casa?', '4', '6', '3', 2)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('Futebol: Qual seleção foi a campeã mundial de 2006?', 'Itália', 'França', 'Alemanha', 1)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('Qual o nome do americano que é considerado o maior golfista de todos os tempos?', 'Tiger Woods', 'Jack Nicklaus', 'Arnold Palmer', 1)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('Quem torce para o São Paulo é?', 'São Paulino', 'Bambi', 'Paulistano', 1)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('Qual o maior vencedor de Grand Slam em simples da história?', 'Roger Federer', 'Rafael Nadal', 'Novak Djokovic', 1)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('Qual o nadador americano recordista de medalhas de ouro nas Olimpíadas?', '´Michael phelps', 'Mark Spitz', 'César Cielo', 1)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('Qual é o elemento mais abundante na Terra?', 'Hidrogênio', 'Oxigênio', 'Hélio', 2)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('Qual destes países é transcontinental?', 'Rússia', 'Istambul', 'Groenlândia', NULL)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('Qual é o nome da unidade de corrente elétrica?', 'Ampere', 'Ohm', 'Volt', 1)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('Qual foi o humilhante placar sofrido pelo Santos contra o corinthians em 2005?', '7x1', '8x2', NULL, 1)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('Qual o maior animal terrestre?', 'Elefante africano', 'Nhonho', 'Girafa', NULL)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('Qual o tema do famoso discurso Eu Tenho um Sonho, de Martin Luther King?', 'Igualdade das raças', 'Justiça para os menos favorecidos', 'Intolerância religiosa', NULL)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('Qual é a fórmula mais famosa de Albert Einstein?', 'E=MC2', 'P=mg', 'F=ma', 2)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('Quantos mundiais tem o palmeiras?', 'Nenhum', '15', '75', 1)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('Qual é o maior planeta do sistema solar?', 'Júpiter', 'Marte', 'Sol', NULL)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('Os ornitorrincos são mamíferos: verdadeiro ou falso?', 'Verdade', 'Falso', NULL, NULL)");
+                stmt.executeUpdate("INSERT INTO questions VALUES ('Que líder mundial ficou conhecida como “Dama de Ferro”?', 'Margaret Thatcher', 'Dilma Rousseff', 'Hillary Clinton', NULL)");
             }
             
             step = "'results' table creation";
